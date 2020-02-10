cbe7f5951f8ab1d3c12c0e4da50bba59e76e3189
vadegg <vadegg0607@gmail.com>
2020-02-10 22:14:16 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
-public class Main {
-    public static void main(String[] args) {
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        String path = "https://ru.wikipedia.org/wiki/%D0%A1%D0%BF%D0%B8%D1%81%D0%BE%D0%BA_%D1%81%D1%82%D0%B0%D0%BD%D1%86%D0%B8%D0%B9_%D0%9C%D0%BE%D1%81%D0%BA%D0%BE%D0%B2%D1%81%D0%BA%D0%BE%D0%B3%D0%BE_%D0%BC%D0%B5%D1%82%D1%80%D0%BE%D0%BF%D0%BE%D0%BB%D0%B8%D1%82%D0%B5%D0%BD%D0%B0";
+
+        Document doc = Jsoup.connect(path).maxBodySize(0).get();
+
+        Elements elements = doc.select("td").select("span");
+        List<String> titles = new ArrayList<>();
+        elements.forEach(element -> {
+            if (element.attr("").contains("линия"))
+            titles.add(element.text());
+        });
+        titles.forEach(System.out::println);
     }
 }
--- src/Parser.java
+++ src/Parser.java
@@ -1,3 +1,16 @@
-public class Parser {
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 
-}
+import java.io.IOException;
+//
+//public class Parser {
+//
+//    String path = "https://ru.wikipedia.org/wiki/%D0%A1%D0%BF%D0%B8%D1%81%D0%BE%D0%BA_%D1%81%D1%82%D0%B0%D0%BD%D1%86%D0%B8%D0%B9_%D0%9C%D0%BE%D1%81%D0%BA%D0%BE%D0%B2%D1%81%D0%BA%D0%BE%D0%B3%D0%BE_%D0%BC%D0%B5%D1%82%D1%80%D0%BE%D0%BF%D0%BE%D0%BB%D0%B8%D1%82%D0%B5%D0%BD%D0%B0";
+//
+//    public Parser() throws IOException {
+//        Document doc = Jsoup.connect(path).get();
+//        Elements elements = doc.select("tb");
+//        elements.forEach(System.out::println);
+//    }
+//}
