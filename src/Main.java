ccfd3ae40834d2949bde71d98dcb7aea490aef35
vadegg <vadegg0607@gmail.com>
2020-03-05 22:31:43 +0300

    oh shit, here we go again

--- src/Main.java
+++ src/Main.java
@@ -75,7 +75,6 @@ public class Main {
                     }
                 }
             }
-            connection
         }
             int size = info.size();
             System.out.println(size);
